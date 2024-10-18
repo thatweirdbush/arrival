2fed6ea897d7fae3307b760a252ec4f864fe2aff
Giangfs <123853667+Giangfs@users.noreply.github.com>
2024-10-18 14:54:39 +0700

    Create new class: User
    use user class and create hard data

---
+++ BookingManagementSystem.Core/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingManagementSystem.Core.Models;
+public class User : INotifyPropertyChanged
+{
+    public string Username
+    {
+        get; set;
+    }
+    public string Password
+    {
+        get; set;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+}
--- BookingManagementSystem/ViewModels/LoginViewModel.cs
+++ BookingManagementSystem/ViewModels/LoginViewModel.cs
@@ -1,9 +1,38 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using BookingManagementSystem.Core.Models;
 
 namespace BookingManagementSystem.ViewModels;
 
 public partial class LoginViewModel : ObservableRecipient
 {
+
+    private ObservableCollection<User> _users
+    {
+        get;
+    } = new()
+    {
+        new User()
+        {
+            Username = "admin",
+            Password = "123"
+        }, 
+        new User()
+        {
+            Username = "customer",
+            Password = "123"
+        }
+    };
+
+    public ObservableCollection<User> Users => _users;
+
+    // Check if user has correct password for account
+    public bool Login(string username, string password)
+    {
+        var user = _users.FirstOrDefault(u => u.Username == username && u.Password == password);
+        return user != null;
+    }
+
     public LoginViewModel()
     {
     }
