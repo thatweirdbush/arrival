a36b2382fa4e025cd609986831e4df53075373f6
Giangfs <123853667+Giangfs@users.noreply.github.com>
2024-11-23 10:31:00 +0700

    partial complete WishlistPage
    Part complete
    - UI
    - process Click Favorite button

--- BookingManagementSystem/ViewModels/Client/WishlistViewModel.cs
+++ BookingManagementSystem/ViewModels/Client/WishlistViewModel.cs
@@ -1,10 +1,93 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using BookingManagementSystem.Contracts.Services;
+using BookingManagementSystem.Core.Contracts.Repositories;
+using BookingManagementSystem.Core.Models;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
 
 namespace BookingManagementSystem.ViewModels.Client;
 
-public partial class WishlistViewModel : ObservableRecipient
+public partial class WishlistViewModel : ObservableRecipient 
 {
-    public WishlistViewModel()
+    private readonly INavigationService _navigationService;
+    private readonly IRepository<Property> _propertyRepository;
+
+    // List of content items
+    public ObservableCollection<Property> Properties { get; set; } = [];
+
+    [ObservableProperty]
+    private bool isPropertyListEmpty;
+
+    public int PropertyCountTotal;
+
+    // List of Property's Name & Location
+    public List<string> PropertyNameAndLocationList
     {
+        get;
+        set;
+    } = [];
+
+    public WishlistViewModel(INavigationService navigationService, IRepository<Property> propertyRepository)
+    {
+        _navigationService = navigationService;
+        _propertyRepository = propertyRepository;
+        CheckPropertyListCount();
+
+        // Subscribe to CollectionChanged event
+        Properties.CollectionChanged += (s, e) => {
+            if (e.NewItems != null)
+            {
+                foreach (Property property in e.NewItems)
+                {
+                    property.PropertyChanged += Property_PropertyChanged;
+                }
+            }
+            if (e.OldItems != null)
+            {
+                foreach (Property property in e.OldItems)
+                {
+                    property.PropertyChanged -= Property_PropertyChanged;
+                }
+            }
+            CheckPropertyListCount();
+        };
+
+        // Initial check
+        LoadPropertyList();
+
+        // Load Property Name and Location string data list
+        PropertyNameAndLocationList = Properties.Select(p => p.Name)
+                                                .Concat(Properties.Select(p => p.Location))
+                                                .ToList();
+        PropertyCountTotal = Properties.Count;
+    }
+
+    private void Property_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(Property.IsFavourite) && sender is Property property)
+        {
+            if (!property.IsFavourite)
+            {
+                Properties.Remove(property);
+            }
+        }
+    }
+
+    public async void LoadPropertyList()
+    {
+        // Load Property data list filtered by User/Host Id
+        var properties = await _propertyRepository.GetAllAsync();
+        foreach (var item in properties.Where(p => p.IsFavourite))
+        {
+            Properties.Add(item);
+        }
+    }
+
+    public void OnNavigatedFrom()
+    {
+    }
+    private void CheckPropertyListCount()
+    {
+        IsPropertyListEmpty = Properties.Count == 0;
     }
 }
--- BookingManagementSystem/Views/Client/WishlistPage.xaml.cs
+++ BookingManagementSystem/Views/Client/WishlistPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using BookingManagementSystem.ViewModels.Client;
+﻿using BookingManagementSystem.Core.Models;
+using BookingManagementSystem.ViewModels.Client;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace BookingManagementSystem.Views.Client;
@@ -14,5 +16,56 @@ public sealed partial class WishlistPage : Page
     {
         ViewModel = App.GetService<WishlistViewModel>();
         InitializeComponent();
+    }
+
+    private void EditListing_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void CancelEditing_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void RemoveListing_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void RemoveAllLissting_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void btnGetStarted_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void SearchListing_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void OnCommandBarElementClicked(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void CloseSearchBoxButton_Click(object sender, RoutedEventArgs e)
+    {
+    }
+    
+    private void btnFavourite_Click(object sender, RoutedEventArgs e)
+    {
+        // Toggle the favourite button  
+        // Change the image source to the filled heart icon  
+        if (sender is FrameworkElement frameworkElement
+            && frameworkElement.DataContext is Property property)
+        {
+            property.IsFavourite = !property.IsFavourite;
+        }
+    }
+
+    private void SearchBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+    {
+    }
+
+    private void SearchBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+    {
     }
 }
